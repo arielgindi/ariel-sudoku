1774478f9e9feb08542791d3221249ea48a766e3
arielgindi <45123820+arielgindi@users.noreply.github.com>
2025-01-10 18:07:05 +0200

    feat: add BoardParser class with Parse method

---
+++ Utils/BoardParser.cs
@@ -0,0 +1,15 @@
+﻿public static class BoardParser
+{
+    public static SudokuBoard Parse(string input)
+    {     
+        SudokuBoard board = new SudokuBoard();
+        for (int i = 0; i < 81; i++)
+        {
+            int row = i / 9;
+            int col = i % 9;
+            board[row, col] = input[i];
+        }
+
+        return board;
+    }
+}
