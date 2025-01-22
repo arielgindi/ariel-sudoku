a7c8ae083655f1f58eceb47d5580dc98eca02b50
arielgindi <45123820+arielgindi@users.noreply.github.com>
2025-01-22 05:38:55 +0200

    test: add medium tests and add an abstract SudokuTestsBase class for code reduction

--- tests/ArielSudoku.Tests/SimpleSudokuTests.cs
+++ tests/ArielSudoku.Tests/SimpleSudokuTests.cs
@@ -1,27 +1,43 @@
-namespace ArielSudoku.Tests
+namespace ArielSudoku.Tests;
+public class EasySudokuTests : SudokuTestsBase
 {
-    public class SudokuTests
+    [Fact]
+    public void SolveEasySudoku1()
     {
-        [Fact]
-        public void SolveSimpleSudoku1()
-        {
-            string puzzle = "100000027000304015500170683430962001900007256006810000040600030012043500058001000";
-            string expectedSolution = "193586427867324915524179683435962871981437256276815349749658132612743598358291764";
+        string puzzle = "100000027000304015500170683430962001900007256006810000040600030012043500058001000";
+        string expectedSolution = "193586427867324915524179683435962871981437256276815349749658132612743598358291764";
+        CheckPuzzleSolution(puzzle, expectedSolution);
+    }
 
-            string actualSolution = SudokuEngine.SolveSudoku(puzzle);
+    [Fact]
+    public void SolveEasySudoku2()
+    {
+        string puzzle = "800000070006010053040600000000080400003000700020005038000000800004050061900002000";
+        string expectedSolution = "831529674796814253542637189159783426483296715627145938365471892274958361918362547";
+        CheckPuzzleSolution(puzzle, expectedSolution);
+    }
 
-            Assert.Equal(expectedSolution, actualSolution);
-        }
+    [Fact]
+    public void SolveEasySudoku3()
+    {
+        string puzzle = "810006050006290000000000004900000320003050100028000005600000000000039400050400037";
+        string expectedSolution = "817346259546298713392715684975184326463952178128673945634827591781539462259461837";
+        CheckPuzzleSolution(puzzle, expectedSolution);
+    }
 
-        [Fact]
-        public void SolveSimpleSudoku2()
-        {
-            string puzzle = "800000070006010053040600000000080400003000700020005038000000800004050061900002000";
-            string expectedSolution = "831529674796814253542637189159783426483296715627145938365471892274958361918362547";
+    [Fact]
+    public void SolveEasySudoku4()
+    {
+        string puzzle = "150369028000000000003000500040000060600701004007050800000090000020405030700000006";
+        string expectedSolution = "154369728978542613263187549542938167689721354317654892431896275826475931795213486";
+        CheckPuzzleSolution(puzzle, expectedSolution);
+    }
 
-            string actualSolution = SudokuEngine.SolveSudoku(puzzle);
-
-            Assert.Equal(expectedSolution, actualSolution);
-        }
+    [Fact]
+    public void SolveEasySudoku5()
+    {
+        string puzzle = "200305008000268000090104030000040000015806740080000050600702001900000003040050020";
+        string expectedSolution = "274395618531268974896174235769543182315826749482917356658732491927481563143659827";
+        CheckPuzzleSolution(puzzle, expectedSolution);
     }
 }
