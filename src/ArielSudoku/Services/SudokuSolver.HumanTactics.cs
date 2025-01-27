fd897122fafb0906887d2f68df060f5b85cbe20e
Ariel Gindi <arielgindi2006@gmail.com>
2025-01-27 13:41:59 +0200

    perf: add another HumanTactic called ApplyNakedSingles

--- src/ArielSudoku/Services/SudokuSolver.HumanTactics.cs
+++ src/ArielSudoku/Services/SudokuSolver.HumanTactics.cs
@@ -2,18 +2,16 @@
 
 public sealed partial class SudokuSolver
 {
-
     private void ApplyHumanTactics(Stack<(int cellIndex, int digit)>? humanTacticsStack)
     {
-        bool isChanged;
-        do
-        {
-            isChanged = ApplyNakedSingles(humanTacticsStack);
-        }
-        while (isChanged);
+        while (ApplyNakedSingles(humanTacticsStack) || ApplyHiddenSingles(humanTacticsStack)) { }
     }
 
-    // Try to place digits in cells that have exactly one possibility
+    /// <summary>
+    /// Try to place digits in cells that have exactly one possibility
+    /// </summary>
+    /// <param name="humanTacticsStack"></param>
+    /// <returns></returns>
     private bool ApplyNakedSingles(Stack<(int cellIndex, int digit)>? humanTacticsStack)
     {
         bool isChanged = false;
@@ -29,6 +27,63 @@ public sealed partial class SudokuSolver
                 humanTacticsStack?.Push((cellIndex, digit));
             }
         }
+        return isChanged;
+    }
+
+    /// <summary>
+    /// Search for cells that there is only one
+    /// possiblie solution it his same (row, col or box)
+    /// For example: if digit 6 is the only possibile digit in that row, it place it there
+    /// </summary>
+    /// <param name="humanTacticsStack"></param>
+    /// <returns></returns>
+    private bool ApplyHiddenSingles(Stack<(int cellIndex, int digit)>? humanTacticsStack)
+    {
+        bool isChanged = false;
+
+        for (int unitIndex = 0; unitIndex < BoardSize; unitIndex++)
+        {
+            isChanged |= FindHiddenSinglesHealper(CellsInRow[unitIndex], humanTacticsStack);
+            isChanged |= FindHiddenSinglesHealper(CellsInCol[unitIndex], humanTacticsStack);
+            isChanged |= FindHiddenSinglesHealper(CellsInBox[unitIndex], humanTacticsStack);
+        }
+
+        return isChanged;
+    }
+
+    /// <summary>
+    /// Healper function for ApplyHiddenSingles
+    /// Given a row, col or box to find a number that exist there only once
+    /// For example: if digit 6 is the only possibile digit in that row, it place it there
+    /// </summary>
+    private bool FindHiddenSinglesHealper(int[] cellsInUnit, Stack<(int cellIndex, int digit)>? humanTacticsStack)
+    {
+        bool isChanged = false;
+
+        for (int digit = 1; digit <= BoardSize; digit++)
+        {
+            int possibleCell = -1;
+            int count = 0;
+
+            // search for each empty cell, if he could be put there place it
+            foreach (int cellIndex in cellsInUnit)
+            {
+                if (_board[cellIndex] == '0' && _board.IsSafeCell(cellIndex, digit))
+                {
+                    possibleCell = cellIndex;
+                    count++;
+                    if (count > 1) break;
+                }
+            }
+
+            if (count == 1)
+            {
+                _board.PlaceDigit(possibleCell, digit);
+                humanTacticsStack?.Push((possibleCell, digit));
+                isChanged = true;
+            }
+        }
+
         return isChanged;
     }
 
